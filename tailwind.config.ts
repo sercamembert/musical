3e50d45d6e958a5611ce4dbc3c2c68bcb6f71182
Filip Maszota <filipmaszota3@gmail.com>
2024-01-09 22:25:42 +0100

    initial configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+        "3xl": "1536px",
+        desktop: "1720px",
+        ultra: "2000px",
+      },
+      colors: {
+        primary: "#FBC403",
+        secoundary: "#FBFCD4",
+        "button-text": "#535347",
+      },
+      fontFamily: {
+        heading: ["Playfair Display", "serif"],
+        headingSC: ["Playfair Display SC", "serif"],
+      },
+    },
+    plugins: ["prettier-plugin-tailwindcss"],
+  },
+};
